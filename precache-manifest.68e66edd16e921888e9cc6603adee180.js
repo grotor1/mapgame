04662f429558fe17f20230da9cac6b5ba060e45b
grotor1 <varukhin04@mail.ru>
2020-10-14 22:50:58 +0300

    Updates

---
+++ precache-manifest.68e66edd16e921888e9cc6603adee180.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3db57e976840179ce671b8a99813a75c",
+    "url": "/mapgame/index.html"
+  },
+  {
+    "revision": "f8f1f05d1922bd003b0c",
+    "url": "/mapgame/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5d110278a4562e00233f",
+    "url": "/mapgame/static/js/2.d5222793.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mapgame/static/js/2.d5222793.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8f1f05d1922bd003b0c",
+    "url": "/mapgame/static/js/main.04a0653e.chunk.js"
+  },
+  {
+    "revision": "4d5e63887f6471465b16",
+    "url": "/mapgame/static/js/runtime-main.47aa3502.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mapgame/static/media/logo.5d5d9eef.svg"
+  }
+]);
