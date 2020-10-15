8d2bc5ba023e5e22d0d5b25fde4fc69de6690597
grotor1 <varukhin04@mail.ru>
2020-10-15 18:31:08 +0300

    deploing app fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Draggable from 'react-draggable';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Чел ты</p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>Чел ты</p>
+                <Draggable>
+                    <p>Чел я</p>
+                </Draggable>
+            </header>
+        </div>
+    );
 }
 
 export default App;
