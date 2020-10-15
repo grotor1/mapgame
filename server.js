1e33a54b7b4b607034b31eb8255632cad931799c
grotor1 <varukhin04@mail.ru>
2020-10-15 10:29:57 +0300

    deploing app fix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // Adds json parsing middleware
 app.use(express.json());
